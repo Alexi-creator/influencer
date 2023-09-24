185af10fcaeceeb96c4b8612d9e853b9f6f87a0f
Ilia Pavlov <pavlov.ru90@gmail.com>
2023-09-24 11:21:22 +0300

    add section layout shop-window

--- src/js/pages/index.ts
+++ src/js/pages/index.ts
@@ -1,10 +1,16 @@
 import '../common.ts'
 import { Share } from '../modules/share'
+import { Collapse } from '../modules/collapse'
 
 window.addEventListener('load', () => {
 
   try {
     new Share()
+  } catch (error) {
+    console.log(error)
+  }
+  try {
+    new Collapse()
   } catch (error) {
     console.log(error)
   }
