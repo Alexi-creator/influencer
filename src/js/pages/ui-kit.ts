594d6a297419ab2a63ad707f73eee897e660295e
Ilia Pavlov <pavlov.ru90@gmail.com>
2024-06-16 11:18:39 +0300

    remove select.ts from ui-kit

--- src/js/pages/ui-kit.ts
+++ src/js/pages/ui-kit.ts
@@ -1,7 +1,6 @@
 import '../common'
 
 import { Collapse } from '../modules/Collapse'
-import { Select } from '../modules/Select'
 import { CustomSwiper } from '../modules/CustomSwiper'
 import { Tabs } from '../modules/Tabs'
 import { RangeSlider } from '../modules/RangeSlider'
@@ -42,11 +41,6 @@ window.addEventListener('load', () => {
   }
   try {
     new Tabs()
-  } catch (error) {
-    console.log(error)
-  }
-  try {
-    new Select()
   } catch (error) {
     console.log(error)
   }
