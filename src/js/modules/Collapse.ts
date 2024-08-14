628aef72d61de855c1a7254eb8bfc166d222bd64
Ilia Pavlov <pavlov.ru90@gmail.com>
2024-08-14 14:30:24 +0300

    done fixes

--- src/js/modules/Collapse.ts
+++ src/js/modules/Collapse.ts
@@ -11,36 +11,40 @@ export class Collapse {
     this.init()
   }
 
-  private init() {
+  private init(): void {
     this.handlers()
   }
 
-  private toggleCollapse(targetElement: HTMLElement) {
+  private toggleCollapse(targetElement: HTMLElement): void {
     const parent = targetElement.closest(this.selector) as HTMLElement
+    const svgIcon = parent.querySelector('.collapse__head-icon use')
 
     if (parent) {
       if (parent.classList.contains('collapse--close')) {
         parent.classList.remove('collapse--close')
         parent.classList.add('collapse--open')
+        svgIcon?.setAttribute('xlink:href', './img/icons/icons.svg#collapse')
       }
       else if (parent?.classList.contains('collapse--open')) {
         parent.classList.remove('collapse--open')
         parent.classList.add('collapse--close')
+        svgIcon?.setAttribute('xlink:href', './img/icons/icons.svg#reveal')
       } else {
         parent.classList.add('collapse--close')
+        svgIcon?.setAttribute('xlink:href', './img/icons/icons.svg#reveal')
       }
     }    
   }
 
-  private clickHandler(e: MouseEvent) {
+  private clickHandler(e: MouseEvent): void {
     const targetElement = e.target as HTMLElement
 
     if (targetElement.closest('.collapse__head') && targetElement.tagName !== 'INPUT') {
-      return this.toggleCollapse(targetElement)
+      this.toggleCollapse(targetElement)
     }
   }
 
-  private handlers() {
+  private handlers(): void {
     document.addEventListener('click', (e) => this.clickHandler(e))
   }
 }
