d6063c7c2daff4d0c7a2a037c56d1bf24f4ffad3
pavlov.i <pavlov.ru90@gmail.com>
2024-10-06 06:39:53 +0300

    refactor popup, add block with products, add mode breadcrumbs

--- src/js/pages/product.ts
+++ src/js/pages/product.ts
@@ -1,9 +1,48 @@
 import '../common'
 
+import { BreakpointWidth } from '../constants'
 import { NumberFormatter } from '../utils/numberFormatter'
 
-try {
-  new NumberFormatter()
-} catch (error) {
-  console.log(error)
-}
+import { CustomSwiper } from '../modules/CustomSwiper'
+
+window.addEventListener('load', () => {
+  try {
+    new NumberFormatter()
+  } catch (error) {
+    console.log(error)
+  }
+  
+  try {
+    document.querySelectorAll('.joint-purchases-card__slider').forEach(swiper => {
+      const swiperElement = swiper.querySelector('.swiper-joint-purchases') as HTMLElement
+      const prevBtn = swiper.querySelector('.swiper-button-prev') as HTMLElement
+      const nextBtn = swiper.querySelector('.swiper-button-next') as HTMLElement      
+  
+      new CustomSwiper({
+        target: swiperElement,
+        options: {
+          slidesPerView: 2,
+          centeredSlides: false,
+          spaceBetween: 12,
+          breakpoints: {
+            [BreakpointWidth.TABLET]: {
+              slidesPerView: 3.5,
+            },
+            [BreakpointWidth.DESKTOP]: {
+              slidesPerView: 2.5,
+            },
+            [BreakpointWidth.FULLHD]: {
+              slidesPerView: 4.5,
+            },
+          },
+        },
+        btnsElements: {
+          prevElement: prevBtn,
+          nextElement: nextBtn,
+        },
+      })
+    })
+  } catch (error) {
+    console.log(error)
+  }
+})
