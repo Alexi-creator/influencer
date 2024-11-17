58b2c36f8ba85e05b605e3d086d4744c6e11988e
pavlov.i <pavlov.ru90@gmail.com>
2024-11-17 12:55:21 +0300

    done publication

--- src/js/pages/publication.ts
+++ src/js/pages/publication.ts
@@ -2,10 +2,18 @@ import '../common'
 
 import { API_URLS, HttpMethods } from '../constants'
 
+import { NumberFormatter } from '../utils/numberFormatter'
+
 import { CustomSwiper } from '../modules/CustomSwiper'
 import { Form } from '../modules/Form'
 
 window.addEventListener('load', () => {
+  try {
+    new NumberFormatter()
+  } catch (error) {
+    console.log(error)
+  }
+
   try {
     new CustomSwiper({
       target: '.swiper-publication',
