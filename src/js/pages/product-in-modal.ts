d0158a86b5614b79ca2162b75539188cd67bf7d3
pavlov.i <pavlov.ru90@gmail.com>
2025-03-27 13:25:59 +0300

    common fix

--- src/js/pages/product-in-modal.ts
+++ src/js/pages/product-in-modal.ts
@@ -1,11 +1,2 @@
 import '../common'
 
-import { Tooltip } from '../modules/Tooltip'
-
-window.addEventListener('load', () => {
-  try {
-    new Tooltip()
-  } catch (error) {
-    console.log(error)
-  }
-})
