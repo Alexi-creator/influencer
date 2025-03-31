ae0e02f9eab7d779b9136afe8f396fb0bbb6e033
Andrey <aebeshten@gmail.com>
2025-03-31 23:17:19 +0300

    feat: add new styles for the jp-details component

---
+++ src/js/pages/inner-jp.ts
@@ -0,0 +1,29 @@
+import '../common'
+
+import { Share } from '../modules/Share'
+import { Collapse } from '../modules/Collapse'
+import { Tabs } from '../modules/Tabs'
+import { Density } from '../modules/Density'
+import { Sort } from '../modules/Sort'
+import { Filter } from '../modules/Filter'
+import { Form } from '../modules/Form'
+import { RangeSlider } from '../modules/RangeSlider'
+import { Masonry } from '../modules/Masonry'
+import { ScrollIntoView } from '../modules/ScrollIntoView'
+import { SyncCategories } from '../modules/SyncCategories'
+import { PreviewToolbarAction } from '../modules/PreviewToolbarAction'
+import { NumberFormatter } from '../utils/numberFormatter'
+
+window.addEventListener('load', () => {
+  try {
+    new Share()
+  } catch (error) {
+    console.log(error)
+  }
+
+  try {
+    new NumberFormatter()
+  } catch (error) {
+    console.log(error)
+  }
+})
